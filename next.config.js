76bf08b3d3c0bfe6830cb44fdcb57e36e4fe62db
Simanchal Nayak <simanchal317@gmail.com>
2026-08-20 23:35:13 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,20 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async headers() {
+  async rewrites() {
     return [
       {
-        // Apply CORS headers to all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // Allows requests from Vercel
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
-        ]
-      }
-    ]
-  }
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
